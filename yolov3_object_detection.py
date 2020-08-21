ff99348b17e76b7eb28e0e2960da5df7fd5e638e
mtclskn <umtclskn@gmail.com>
2020-08-21 16:35:06 +0300

    refactor: deleted unused code blocks

--- yolov3_object_detection.py
+++ yolov3_object_detection.py
@@ -74,143 +74,6 @@ VIEW_FOV = 90
 BB_COLOR = (248, 64, 24)
 
 # ==============================================================================
-# -- ClientSideBoundingBoxes ---------------------------------------------------
-# ==============================================================================
-
-
-class ClientSideBoundingBoxes(object):
-    """
-    This is a module responsible for creating 3D bounding boxes and drawing them
-    client-side on pygame surface.
-    """
-
-    @staticmethod
-    def get_bounding_boxes(vehicles, camera):
-        """
-        Creates 3D bounding boxes based on carla vehicle list and camera.
-        """
-
-        bounding_boxes = [ClientSideBoundingBoxes.get_bounding_box(vehicle, camera) for vehicle in vehicles]
-        # filter objects behind camera
-        bounding_boxes = [bb for bb in bounding_boxes if all(bb[:, 2] > 0)]
-        return bounding_boxes
-
-    @staticmethod
-    def draw_bounding_boxes(display, bounding_boxes):
-        """
-        Draws bounding boxes on pygame display.
-        """
-
-        bb_surface = pygame.Surface((VIEW_WIDTH, VIEW_HEIGHT))
-        bb_surface.set_colorkey((0, 0, 0))
-        for bbox in bounding_boxes:
-            points = [(int(bbox[i, 0]), int(bbox[i, 1])) for i in range(8)]
-            # draw lines
-            # base
-            pygame.draw.line(bb_surface, BB_COLOR, points[0], points[1])
-            pygame.draw.line(bb_surface, BB_COLOR, points[0], points[1])
-            pygame.draw.line(bb_surface, BB_COLOR, points[1], points[2])
-            pygame.draw.line(bb_surface, BB_COLOR, points[2], points[3])
-            pygame.draw.line(bb_surface, BB_COLOR, points[3], points[0])
-
-        display.blit(bb_surface, (0, 0))
-
-    @staticmethod
-    def get_bounding_box(vehicle, camera):
-        """
-        Returns 3D bounding box for a vehicle based on camera view.
-        """
-
-        bb_cords = ClientSideBoundingBoxes._create_bb_points(vehicle)
-        cords_x_y_z = ClientSideBoundingBoxes._vehicle_to_sensor(bb_cords, vehicle, camera)[:3, :]
-        cords_y_minus_z_x = np.concatenate([cords_x_y_z[1, :], -cords_x_y_z[2, :], cords_x_y_z[0, :]])
-        bbox = np.transpose(np.dot(camera.calibration, cords_y_minus_z_x))
-        camera_bbox = np.concatenate([bbox[:, 0] / bbox[:, 2], bbox[:, 1] / bbox[:, 2], bbox[:, 2]], axis=1)
-        return camera_bbox
-
-    @staticmethod
-    def _create_bb_points(vehicle):
-        """
-        Returns 3D bounding box for a vehicle.
-        """
-
-        cords = np.zeros((8, 4))
-        extent = vehicle.bounding_box.extent
-        cords[0, :] = np.array([extent.x, extent.y, -extent.z, 1])
-        cords[1, :] = np.array([-extent.x, extent.y, -extent.z, 1])
-        cords[2, :] = np.array([-extent.x, -extent.y, -extent.z, 1])
-        cords[3, :] = np.array([extent.x, -extent.y, -extent.z, 1])
-        cords[4, :] = np.array([extent.x, extent.y, extent.z, 1])
-        cords[5, :] = np.array([-extent.x, extent.y, extent.z, 1])
-        cords[6, :] = np.array([-extent.x, -extent.y, extent.z, 1])
-        cords[7, :] = np.array([extent.x, -extent.y, extent.z, 1])
-        return cords
-
-    @staticmethod
-    def _vehicle_to_sensor(cords, vehicle, sensor):
-        """
-        Transforms coordinates of a vehicle bounding box to sensor.
-        """
-
-        world_cord = ClientSideBoundingBoxes._vehicle_to_world(cords, vehicle)
-        sensor_cord = ClientSideBoundingBoxes._world_to_sensor(world_cord, sensor)
-        return sensor_cord
-
-    @staticmethod
-    def _vehicle_to_world(cords, vehicle):
-        """
-        Transforms coordinates of a vehicle bounding box to world.
-        """
-
-        bb_transform = carla.Transform(vehicle.bounding_box.location)
-        bb_vehicle_matrix = ClientSideBoundingBoxes.get_matrix(bb_transform)
-        vehicle_world_matrix = ClientSideBoundingBoxes.get_matrix(vehicle.get_transform())
-        bb_world_matrix = np.dot(vehicle_world_matrix, bb_vehicle_matrix)
-        world_cords = np.dot(bb_world_matrix, np.transpose(cords))
-        return world_cords
-
-    @staticmethod
-    def _world_to_sensor(cords, sensor):
-        """
-        Transforms world coordinates to sensor.
-        """
-
-        sensor_world_matrix = ClientSideBoundingBoxes.get_matrix(sensor.get_transform())
-        world_sensor_matrix = np.linalg.inv(sensor_world_matrix)
-        sensor_cords = np.dot(world_sensor_matrix, cords)
-        return sensor_cords
-
-    @staticmethod
-    def get_matrix(transform):
-        """
-        Creates matrix from carla transform.
-        """
-
-        rotation = transform.rotation
-        location = transform.location
-        c_y = np.cos(np.radians(rotation.yaw))
-        s_y = np.sin(np.radians(rotation.yaw))
-        c_r = np.cos(np.radians(rotation.roll))
-        s_r = np.sin(np.radians(rotation.roll))
-        c_p = np.cos(np.radians(rotation.pitch))
-        s_p = np.sin(np.radians(rotation.pitch))
-        matrix = np.matrix(np.identity(4))
-        matrix[0, 3] = location.x
-        matrix[1, 3] = location.y
-        matrix[2, 3] = location.z
-        matrix[0, 0] = c_p * c_y
-        matrix[0, 1] = c_y * s_p * s_r - s_y * c_r
-        matrix[0, 2] = -c_y * s_p * c_r - s_y * s_r
-        matrix[1, 0] = s_y * c_p
-        matrix[1, 1] = s_y * s_p * s_r + c_y * c_r
-        matrix[1, 2] = -s_y * s_p * c_r + c_y * s_r
-        matrix[2, 0] = s_p
-        matrix[2, 1] = -c_p * s_r
-        matrix[2, 2] = c_p * c_r
-        return matrix
-
-
-# ==============================================================================
 # -- BasicSynchronousClient ----------------------------------------------------
 # ==============================================================================
 
@@ -322,7 +185,7 @@ class BasicSynchronousClient(object):
             self.image = img
             self.capture = False
 
-    def render(self, display, sess, num_classes, input_size, graph, return_tensors):
+    def render(self, display):
         """
         Transforms image from camera sensor and blits it to main pygame display.
         """
@@ -333,26 +196,6 @@ class BasicSynchronousClient(object):
             array = array[:, :, :3]
             array = array[:, :, ::-1]
             self.raw_image = cv2.cvtColor(array,cv2.COLOR_BGR2RGB)
-            
-            '''
-            frame_size = self.raw_image.shape[:2]
-            image_data = utils.image_preporcess(np.copy(self.raw_image), [input_size, input_size])
-            image_data = image_data[np.newaxis, ...]
-    
-            pred_sbbox, pred_mbbox, pred_lbbox = sess.run(
-                [return_tensors[1], return_tensors[2], return_tensors[3]],
-                        feed_dict={ return_tensors[0]: image_data})
-    
-            pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-                                        np.reshape(pred_mbbox, (-1, 5 + num_classes)),
-                                        np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
-    
-            bboxes = utils.postprocess_boxes(pred_bbox, frame_size, input_size, 0.3)
-            bboxes = utils.nms(bboxes, 0.45, method='nms')
-            img = utils.draw_bbox(self.raw_image, bboxes)
-            
-            cv2.imshow("test", img)
-            '''
             surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
             display.blit(surface, (0, 0))
 
@@ -376,7 +219,6 @@ class BasicSynchronousClient(object):
             pygame_clock = pygame.time.Clock()
 
             self.set_synchronous_mode(True)
-            #vehicles = self.world.get_actors().filter('vehicle.*')
             
             with tf.Session(graph=graph) as sess:
                 while True:
@@ -385,8 +227,7 @@ class BasicSynchronousClient(object):
                     self.capture = True
                     pygame_clock.tick_busy_loop(20)
     
-                    self.render(self.display, sess, num_classes, input_size, graph, return_tensors)
-                    #bounding_boxes = ClientSideBoundingBoxes.get_bounding_boxes(vehicles, self.camera)
+                    self.render(self.display)
                     
                     self.raw_image = cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2RGB)
                     frame_size = self.raw_image.shape[:2]
@@ -405,8 +246,6 @@ class BasicSynchronousClient(object):
                     bboxes = utils.nms(bboxes, 0.45, method='nms')
                     utils.draw_bounding_boxes(pygame, self.display,  self.raw_image, bboxes)
                     
-                    #ClientSideBoundingBoxes.draw_bounding_boxes(self.display, bounding_boxes)
-               
                     pygame.display.flip()
     
                     pygame.event.pump()
@@ -449,80 +288,6 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-'''
-def game_loop(args, num_classes, input_size, graph, return_tensors):
-    pygame.init()
-    pygame.font.init()
-    world = None
-
-    try:
-        client = carla.Client(args.host, args.port)
-        client.set_timeout(2.0)
-
-        display = pygame.display.set_mode(
-            (args.width, args.height),
-            pygame.HWSURFACE | pygame.DOUBLEBUF)
-
-        hud = HUD(args.width, args.height)
-        world = World(client.get_world(), hud, args)
-        controller = KeyboardControl(world, args.autopilot)
-
-        clock = pygame.time.Clock()
-        with tf.Session(graph=graph) as sess:
-            while True:
-                clock.tick_busy_loop(60)
-                if controller.parse_events(client, world, clock):
-                    return
-                world.tick(clock)
-                
-                image = world.camera_manager.image
-                array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-                array = np.reshape(array, (image.height,image.width, 4))
-                array = array[:, :, :3]
-                array = array[:, :, ::-1]
-                image = cv2.cvtColor(array,cv2.COLOR_BGR2RGB)
-                #cv2.imshow("test",image)
-                                                
-     
-                image_size = image.shape[:2]
-                image_data = utils.image_preporcess(np.copy(image), [input_size, input_size])
-                image_data = image_data[np.newaxis, ...]
-
-        
-                pred_sbbox, pred_mbbox, pred_lbbox = sess.run(
-                    [return_tensors[1], return_tensors[2], return_tensors[3]],
-                            feed_dict={ return_tensors[0]: image_data})
-        
-                pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-                                            np.reshape(pred_mbbox, (-1, 5 + num_classes)),
-                                            np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
-        
-                bboxes = utils.postprocess_boxes(pred_bbox, image_size, input_size, 0.3)
-                bboxes = utils.nms(bboxes, 0.45, method='nms')
-                print("bboxes:", bboxes)    
-                
-                
-                if len(bboxes) > 0:
-                    image = utils.draw_bbox(image, bboxes)
-                    cv2.imshow("vehicle")
-                
-                world.render(display)
-                pygame.display.flip()
- 
-                
-
-    finally:
-
-        if (world and world.recording_enabled):
-            client.stop_recorder()
-
-        if world is not None:
-            world.destroy()
-
-        pygame.quit()
-
-'''
 
 
 
